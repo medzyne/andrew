e46b1a0aa8a3c759f08bd19fcf85d2d1e5151f98
liminal18 <aljones15@gmail.com>
2016-02-07 22:21:52 +0700

    ok it works

--- dbhandler/upload_iphone.php
+++ dbhandler/upload_iphone.php
@@ -8,21 +8,16 @@ $photo_name = $file["name"];
 $photo_path = $file["tmp_name"];
 $shop_id = $_SESSION['shop_id'];
 $section = "iphoneBackground";
-$countOuery = "SELECT shop_style FROM shop_detail WHERE shop_id = '$shopID'";
+$countOuery = "SELECT shop_style FROM shop_detail WHERE shop_id = '$shop_id'";
 $styleID = mysql_result(mysql_query($countOuery), 0);
 
 if($shop_id && $styleID){
   if(save_file($shop_id, $photo_name, $photo_path, $section))
   {
-    $save_photo_query = "UPDATE shop_style SET shop_bg_image = '$photo_name' WHERE style_id = $styleID";
-    if(mysql_result($save_photo_query)){
+    $save_photo_query = "UPDATE shop_style SET shop_bg_image = '$photo_name' WHERE shop_style_id = '$styleID'";
+    if(mysql_query($save_photo_query)){
       http_response_code(200);
       echo(json_encode(array("message" => "style_photo_updated", "shopID" => $shop_id, "styleID" => $styleID)));
-      exit();
-    }
-    else{
-      http_response_code(400);
-      echo(mysql_error());
       exit();
     }
   }
@@ -34,14 +29,12 @@ if($shop_id && !$styleID)
     $newStyleQuery = "INSERT INTO shop_style (shop_bg_image) VALUES ('$photo_name')";
     if(mysql_query($newStyleQuery)){
       $styleID = mysql_insert_id();
-      http_response_code(200);
-      echo(json_encode(array("message" => "style_photo_updated", "shopID" => $shop_id, "styleID" => $styleID)));
-      exit();
-  }
-  else{
-    http_response_code(400);
-    echo(mysql_error());
-    exit();
+      $addStyleToShop = "UPDATE shop_detail SET shop_style = '$styleID' WHERE shop_id = '$shop_id'";
+      if(mysql_query($addStyleToShop)){
+        http_response_code(200);
+        echo(json_encode(array("message" => "style_photo_created", "shopID" => $shop_id, "styleID" => $styleID)));
+        exit();
+    }
   }
   }
 }
