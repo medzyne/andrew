046ddbd14228816d52caf5a1be3941f46bf54cc7
liminal18 <aljones15@gmail.com>
2016-02-24 11:57:35 +0700

    better serving fetching

--- dbhandler/requestapp.php
+++ dbhandler/requestapp.php
@@ -20,6 +20,46 @@ JOIN shop_call ON shop_detail.shop_id = shop_call.shop_id
 JOIN shop_video ON shop_detail.shop_id = shop_video.shop_id
 WHERE shop_detail.shop_id = $shop_id";
 
+$table_names =
+array("shop_detail","shop_activity",
+"shop_album","shop_call",
+"shop_loralty_card","shop_notification",
+"shop_promotion", "shop_video"
+);
+
+function make_shopId_query($table_name, $shop_id){
+  return "SELECT * FROM $table_name WHERE shop_id=$shop_id";
+}
+
+function add_shop_ids($list, $id)
+{
+  $ids = array();
+  foreach($list as $item){
+    array_push($ids, $id);
+  }
+  return $ids;
+}
+
+$table_names = array_map("make_shopId_query", $table_names,
+add_shop_ids($table_names, $shop_id));
+
+$results = array();
+
+foreach($table_names as $query)
+{
+  $mysql_result = mysql_query($query);
+  if($mysql_result){
+    while($r = mysql_fetch_assoc($mysql_result))
+    {
+      array_push($results, $r);
+    }
+  }
+}
+
+http_response_code(200);
+echo(json_encode($results));
+exit();
+
 $theQuery = mysql_query($getAll);
 if($theQuery)
 {
